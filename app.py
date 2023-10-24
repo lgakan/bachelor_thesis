32e473b7343c876be33128422099c6a242dbb88c
lgakan <lgakan@student.agh.edu.pl>
2023-10-25 01:10:12 +0200

    app_first_version

--- app.py
+++ app.py
@@ -20,35 +20,43 @@ def interactive_plot(df: pd.DataFrame):
     st.plotly_chart(fig, use_container_width=True)
 
 
-# st.set_page_config(page_title="RES", page_icon=":bar_chart:", layout="wide")
 st.set_page_config(page_title="RES", page_icon=":bar_chart:")
-
 st.title("Energy Management System")
-st.markdown("_Prototype v0.4.1_")
 
-with st.sidebar:
+with st.form("my_form"):
     date_start = datetime.strptime("04.09.2020 05:00:00", "%d.%m.%Y %H:%M:%S")
     date_end = datetime.strptime("04.09.2020 12:00:00", "%d.%m.%Y %H:%M:%S")
     date_earliest = datetime.strptime("01.09.2020 05:00:00", "%d.%m.%Y %H:%M:%S")
-    date_oldest = datetime.strptime("20.09.2020 12:00:00", "%d.%m.%Y %H:%M:%S")
+    date_oldest = datetime.strptime("10.09.2020 12:00:00", "%d.%m.%Y %H:%M:%S")
     date_start, date_end = st.slider("Date range",
                                      min_value=date_earliest,
                                      max_value=date_oldest,
                                      value=[date_start, date_end])
     energy_bank_max_lvl = st.number_input("Energy bank max lvl:", min_value=0.0, max_value=5.0, value=3.0)
+    day_algorithm_choice = st.selectbox("Chose a day algorithm", ("full_bank", "interval"))
+    with st.expander("Info about day algorithms"):
+        st.markdown("""##### *full_bank* \n\n _The priority of this algorithm is to ensure the maximum energy bank level
+                    during the day. If consumption exceeds production, and there's a need to source additional energy, 
+                    the system will prefer to purchase it. It will only use the energy bank when it is certain that it 
+                    will be fully charged by the end of the day.This algorithm works best when prices fluctuate 
+                    unpredictably._""")
+        st.markdown("""##### *interval* \n\n _The goal of the algorithm is to utilize the time between the appearance of 
+                    energy prices for a new day and sunset (the moment when photovoltaic production stops). During this 
+                    period, system calculates the optimal plan for using energy from the energy bank until the next 
+                    morning. This algorithm is particularly effective during the summer when sunset occurs later._""")
+
+    submitted = st.form_submit_button("Run!")
+    if submitted:
+        smart_system = SmartSystem()
+        for current_date in pd.date_range(start=date_start, end=date_end, freq=timedelta(hours=1)):
+            logger.info(f"CURRENT DATE: {current_date}")
+            smart_system.feed_consumption(current_date)
 
 with st.container():
-    with st.expander("Chosen System Info"):
-        st.write(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Id venenatis a condimentum vitae. Felis donec et odio pellentesque diam volutpat. Diam phasellus vestibulum lorem sed risus. Turpis egestas sed tempus urna et pharetra pharetra. Commodo quis imperdiet massa tincidunt nunc. Diam vulputate ut pharetra sit amet aliquam id. Gravida neque convallis a cras. Porttitor lacus luctus accumsan tortor.")
-
-    smart_system = SmartSystem()
-    st.write(SmartSystem.__name__)
-    for current_date in pd.date_range(start=date_start, end=date_end, freq=timedelta(hours=1)):
-        logger.info(f"CURRENT DATE: {current_date}")
-        smart_system.feed_consumption(current_date)
-
-    st.write(f"TOTAL: {smart_system.summed_cost:.2}")
-    x = smart_system.plot_charts()
-    print(x)
-    interactive_plot(x)
+    try:
+        st.write(f"TOTAL: {smart_system.summed_cost:.2}")
+        df_to_plot = smart_system.plot_charts()
+        interactive_plot(df_to_plot)
+    except:
+        st.info('Click "Run!" button run the system!')
+        st.stop()
