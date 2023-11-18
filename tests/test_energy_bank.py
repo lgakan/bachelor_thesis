d0ad5f72eed647beb65a7fd4c7e278df15c70703
lgakan <lgakan@student.agh.edu.pl>
2023-11-18 12:41:59 +0100

    EnergyBank tests

---
+++ tests/test_energy_bank.py
@@ -0,0 +1,63 @@
+from scripts.energy_bank import EnergyBank
+import pytest
+
+
+class TestEnergyBank:
+    CAPACITY = 5.0
+    LVL = 2.0
+    MIN_LVL = 0.5
+    COST = 1000.0
+    CYCLES = 1000
+
+    @pytest.fixture(scope="function")
+    def energy_bank(self) -> EnergyBank:
+        return EnergyBank(capacity=self.CAPACITY,
+                          min_lvl=self.MIN_LVL,
+                          lvl=self.LVL,
+                          purchase_cost=self.COST,
+                          cycles_num=self.CYCLES)
+
+    def test_min_border(self, energy_bank: EnergyBank) -> None:
+        energy_bank.lvl = energy_bank.min_lvl
+        energy_bank.manage_energy(-1.0)
+        assert energy_bank.lvl == energy_bank.min_lvl
+
+    def test_max_border(self, energy_bank: EnergyBank) -> None:
+        energy_bank.lvl = energy_bank.capacity
+        energy_bank.manage_energy(1.0)
+        assert energy_bank.lvl == energy_bank.capacity
+
+    @pytest.mark.parametrize("balance_in, expected_lvl", [(-1.0, LVL-1.0), (-LVL, 0.0), (-(CAPACITY+1.0), MIN_LVL)])
+    def test_negative_balances(self, balance_in: float, expected_lvl: float, energy_bank: EnergyBank) -> None:
+        energy_bank.manage_energy(balance_in)
+        assert energy_bank.lvl == expected_lvl
+
+    @pytest.mark.parametrize("balance_in, expected_lvl", [(1.0, LVL+1.0), ((CAPACITY-LVL), CAPACITY), (CAPACITY+1.0, CAPACITY)])
+    def test_positive_balances(self, balance_in: float, expected_lvl: float, energy_bank: EnergyBank) -> None:
+        energy_bank.manage_energy(balance_in)
+        assert energy_bank.lvl == expected_lvl
+
+    @pytest.mark.parametrize("cost_in, cycles_in, balance_input,expected_cost", [(1.0, 1, 5, 2.5),
+                                                                                 (400.0, 5.0, 5, 200),
+                                                                                 (20.0, 40, 10, 2.5)])
+    def test_operation_cost_calculating(self, cost_in: float, cycles_in: int, balance_input: float, expected_cost: float):
+        energy_bank = EnergyBank(purchase_cost=cost_in, cycles_num=cycles_in)
+        assert energy_bank.operation_cost(balance_input) == expected_cost
+        assert energy_bank.operation_cost(-balance_input) == expected_cost
+
+    def test_idle_input(self, energy_bank: EnergyBank):
+        previous_energy_lvl = energy_bank.lvl
+        energy_bank.manage_energy(0)
+        assert previous_energy_lvl == energy_bank.lvl
+
+    @pytest.mark.parametrize("invalid_new_lvl", [-2 * CAPACITY, 2 * CAPACITY, 'LVL', '11'])
+    def test_invalid_lvl(self, invalid_new_lvl, energy_bank: EnergyBank):
+        with pytest.raises(Exception):
+            energy_bank.lvl = invalid_new_lvl
+
+    def test_same_operation_cost(self, energy_bank: EnergyBank):
+        balance_in = 5.0
+        operation_cost = energy_bank.operation_cost(balance_in)
+        for _ in range(10):
+            energy_bank.operation_cost(balance_in)
+        assert operation_cost == energy_bank.operation_cost(balance_in)
